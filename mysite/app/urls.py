337fdf2cf9197ed237909a3d34fd94c00d3be418
Gautam Anil <gautamanil20july@gmail.com>
2020-02-06 15:14:05 +0530

    fixed big

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('' , views.render_home_page) ,
     path('index/', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('index/<int:question_id>/', views.detail, name='detail'),
+    path('index/<int:question_id>/results/', views.results, name='results'),
+    path('index/<int:question_id>/vote/', views.vote, name='vote'),
 ]
